14b57dc0ad707d4e483cc03726f01a6635dc7280
seanaleid <seanaleid@gmail.com>
2020-07-16 10:32:27 -0400

    MVP for binary_search_tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -18,44 +18,99 @@ class BSTNode:
     # Insert the given value into the tree
     def insert(self, value):
         pass
+        # Case 1: value is less than self.value
+        if value < self.value:
+            # If there is no left child, insert value here
+            if self.left is None:
+                self.left = BSTNode(value)
+            # Else 
+            else:
+                # Repeat the process on left subtree
+                self.left.insert(value)
+        # Case 2: value is greater than or equal self.value
+        elif value >= self.value:
+            # if there is no right childe, insert value here
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                # repeat the process on right subtree
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # Case 1: self.value is equal to the target
+        if self.value == target:
+            return True
+        # Case 2: target is less than self.value
+        if target < self.value:
+            # if self.left is None, it isn't in the tree
+            if self.left is None:
+                return False
+            else:
+                return self.right.contains(target)
+        # Case 3: otherwise
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # check is self.right is None
+        # if self.right is None, return self.value
+        # else self.right.get_max(self)
+        if self.right:
+            return self.right.get_max()
+        return self.value
+
+        # if self.value == None:
+        #     return self.value
+        # else:
+        #     return self.get_max(self.right)
+            
+        # print(type(self.value))
+        # print(type(self.right))
+        # if self.value is not None:
+        #     if self.value < self.right:
+        #         return self.right.get_max()
+        #     else: 
+        #         return self.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.right:
+            self.right.for_each(fn)
+        if self.left:
+            self.left.for_each(fn)
+        
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
+    # - def in_order_print(self, node):
+    #     pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self, node):
-        pass
+    # - def bft_print(self, node):
+    #     pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self, node):
-        pass
+    # - def dft_print(self, node):
+    #     pass
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
+    # def pre_order_dft(self, node):
+    #     pass
 
     # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
+    # def post_order_dft(self, node):
+    #     pass
